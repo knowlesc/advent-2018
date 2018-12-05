065d0a161e0a7f1c6d96c169627dee80f26c3855
knowlesc <knowlesc8@gmail.com>
2018-12-05 01:38:05 -0800

    Add day 3 solutions

---
+++ build/day3.js
@@ -0,0 +1,78 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+var solver_1 = require("./solver");
+function generateFabricArray(input) {
+    var fabricSize = 1000;
+    var fabric = [];
+    for (var y = 0; y < fabricSize; y++) {
+        fabric.push([]);
+        for (var x = 0; x < fabricSize; x++) {
+            fabric[y].push(0);
+        }
+    }
+    input.forEach(function (claim) {
+        for (var i = claim.top; i < claim.top + claim.height; i++) {
+            for (var j = claim.left; j < claim.left + claim.width; j++) {
+                fabric[i][j]++;
+            }
+        }
+    });
+    return fabric;
+}
+var Day3 = /** @class */ (function (_super) {
+    __extends(Day3, _super);
+    function Day3() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.inputFile = './inputs/day3.txt';
+        _this.solutions = [
+            function part1(input) {
+                return generateFabricArray(input)
+                    .map(function (row) { return row.reduce(function (sum, i) { return sum + (i >= 2 ? 1 : 0); }); })
+                    .reduce(function (sum, i) { return sum + i; });
+            },
+            function part2(input) {
+                var fabric = generateFabricArray(input);
+                var answer = null;
+                input.some(function (claim) {
+                    for (var i = claim.top; i < claim.top + claim.height; i++) {
+                        for (var j = claim.left; j < claim.left + claim.width; j++) {
+                            if (fabric[i][j] > 1) {
+                                return false;
+                            }
+                        }
+                    }
+                    answer = claim.id;
+                    return true;
+                });
+                return answer;
+            }
+        ];
+        return _this;
+    }
+    Day3.prototype.formatInput = function (input) {
+        return input.map(function (i) {
+            var groups = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/.exec(i);
+            return {
+                id: parseInt(groups[1]),
+                left: parseInt(groups[2]),
+                top: parseInt(groups[3]),
+                width: parseInt(groups[4]),
+                height: parseInt(groups[5])
+            };
+        });
+    };
+    return Day3;
+}(solver_1.Solver));
+exports.Day3 = Day3;
+new Day3().run();
+//# sourceMappingURL=day3.js.map
---
+++ src/day3.ts
@@ -0,0 +1,79 @@
+import { Solver } from './solver';
+
+interface claim {
+  id: number;
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+}
+
+function generateFabricArray(input: claim[]) {
+  const fabricSize = 1000;
+  const fabric: number[][] = [];
+
+  for (let y = 0; y < fabricSize; y++) {
+    fabric.push([]);
+    for (let x = 0; x < fabricSize; x++) {
+      fabric[y].push(0);
+    }
+  }
+
+  input.forEach((claim) => {
+    for (let i = claim.top; i < claim.top + claim.height; i++) {
+      for (let j = claim.left; j < claim.left + claim.width; j++) {
+        fabric[i][j]++;
+      }
+    }
+  });
+
+  return fabric;
+}
+
+export class Day3 extends Solver<claim> {
+  inputFile = './inputs/day3.txt';
+
+  formatInput(input: string[]) {
+    return input.map((i) => {
+      const groups = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/.exec(i);
+
+      return <claim>{
+        id: parseInt(groups[1]),
+        left: parseInt(groups[2]),
+        top: parseInt(groups[3]),
+        width: parseInt(groups[4]),
+        height: parseInt(groups[5])
+      }
+    });
+  }
+
+  solutions = [
+    function part1(input: claim[]) {
+      return generateFabricArray(input)
+        .map((row) => row.reduce((sum, i) => sum + (i >= 2 ? 1 : 0)))
+        .reduce((sum, i) => sum + i);
+    },
+
+    function part2(input: claim[]) {
+      const fabric = generateFabricArray(input);
+
+      let answer: number = null;
+      input.some((claim) => {
+        for (let i = claim.top; i < claim.top + claim.height; i++) {
+          for (let j = claim.left; j < claim.left + claim.width; j++) {
+            if (fabric[i][j] > 1) {
+              return false;
+            }
+          }
+        }
+
+        answer = claim.id;
+        return true;
+      });
+
+      return answer;
+    }
+  ];
+}
+
+new Day3().run();
